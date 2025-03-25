11f8e52db7011eb70b22f30e24937b8181a0e905
SwarnSinghPatel <160294941+SwarnSinghPatel@users.noreply.github.com>
2025-03-25 04:29:13 +0000

    variables

---
+++ 01_variables/vari.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "swarnceo@gamil.com"
+var accountPassword = "123sw"
+
+accountCity = "Rewa"
+
+let accountState;
+
+// accountId = 4
+// this is not changeable because it is the constant variables.
+
+
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+// "prefer not to use the var always use the let and the const."
